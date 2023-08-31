b04300af23fe39b0d7ce34ddcec8d6ba8efcc0b9
Peggy Fan <PeggyFan@users.noreply.github.com>
2023-08-31 10:43:59 -0700

    Update podcast_frontend.py
    testing

--- podcast_frontend.py
+++ podcast_frontend.py
@@ -25,7 +25,8 @@ def main():
         st.subheader("Episode Title")
         st.write(podcast_info['podcast_details']['episode_title'])
         st.subheader("Date")
-        st.write(podcast_info['podcast_details']['episode_pubDate'])
+        st.write(podcast_info['podcast_details'].keys())
+                 #['episode_pubDate'])
 
         # Display the podcast summary and the cover image in a side-by-side layout
         col1, col2 = st.columns([7, 3])
